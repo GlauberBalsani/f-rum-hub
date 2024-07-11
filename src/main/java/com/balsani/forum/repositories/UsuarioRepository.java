762be96698b62b1aa5b7b2ad287a31e700e460ee
GlauberBalsani <glauber_29@hotmail.com>
2024-07-11 16:46:30 -0300

    adicionando service do topico

---
+++ src/main/java/com/balsani/forum/repositories/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.balsani.forum.repositories;
+
+import com.balsani.forum.domain.usuario.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
---
+++ src/main/java/com/balsani/forum/services/UsuarioService.java
@@ -0,0 +1,16 @@
+package com.balsani.forum.services;
+
+import com.balsani.forum.repositories.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+}
